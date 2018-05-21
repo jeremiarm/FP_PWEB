0c6174f1e28382e54d447f815fd3bb4c068115f6
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2018-05-21 20:41:28 +0700

    login dulu

---
+++ ATT/app/Http/Controllers/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validitator;
+use Auth;
+
+class LoginController extends Controller
+{
+	public function username(){
+		return 'username';
+	}
+	
+    public function index()
+    {
+    	//dd($request->all());
+    	return view('login');
+    }
+
+    public function checklogin(Request $request)
+    {
+    	//dd($request->all());
+    	$validate=$request->validate([
+    		'username' => 'required',
+    		'password' => 'required',
+    	]);
+    	if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
+            return redirect('/');
+        }
+        else
+        {
+        	return back()->with('error','Wrong Login Details');
+        }
+    }
+
+    public function successlogin(){
+    	return view('welcome');
+    }
+
+    public function logout(){
+    	Auth::logout();
+    	return redirect('/login');
+    }
+}
+
---
+++ ATT/database/migrations/2018_05_21_121446_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('username');
+            $table->string('password');
+            $table->string('email');
+            $table->string('name');
+            $table->string('address');
+            $table->binary('Picture');
+            $table->date('birth_date');
+            $table->integer('rank');
+            $table->integer('currprobsolv');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
