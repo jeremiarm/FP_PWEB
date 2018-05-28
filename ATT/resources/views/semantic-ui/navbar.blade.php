760c4dc50896f34ffad150e6ad10298d565608f3
Unknown <drdennyr1245@gmail.com>
2018-05-28 19:20:38 +0700

    Update
    dah tak update register,login, sama main page, tapi routing nya dari punya gk tak push, konflik sama punyamu nanti

---
+++ ATT/resources/views/semantic-ui/navbar.blade.php
@@ -0,0 +1,21 @@
+
+<div class="ui inverted menu">
+	<a class="active item" href="{{route('index')}}">
+		Home
+	</a>
+	<a class="item">
+		Messages
+	</a>
+	<a class="item">
+		Friends
+	</a>
+	<div class="right menu">
+		<a class="ui item" href="{{route('login')}}">
+			Login
+		</a>
+		<a class="ui item" href="{{route('register')}}">
+			Register
+		</a>
+	</div>
+</div>
+</div>
