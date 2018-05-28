5527d98ba0b79b404447998b46ded8df2ac491b4
Unknown <drdennyr1245@gmail.com>
2018-05-28 22:59:16 +0700

    hehehehe

---
+++ ATT/resources/views/semantic-ui/navgame.blade.php
@@ -0,0 +1,19 @@
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background:linear-gradient(to bottom,rgba(0, 0, 0,1),rgba(0,0,0,0.2));">
+		<p class="navbar-brand" style="font-size: 25px">[Username]</p>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<p class="nav-link" style="font-size: 20px">Rank : [rank]
+					</li>
+					<li class="nav-item">
+						<p class="nav-link" style="font-size: 20px">Solved Problem : [solvedproblem]</p>
+					</li>
+					</li>
+				</ul>
+			</div>
+		</nav>
